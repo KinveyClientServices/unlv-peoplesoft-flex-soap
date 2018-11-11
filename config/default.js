4b3259e9788ed0c0d35916c832abcbff9de7efc2
Jai Karve <jai.karve@gmail.com>
2018-11-10 17:36:40 -0800

    Initial commit

---
+++ config/default.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2016 Kinvey Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+module.exports = {
+    peopleSoft: {
+        connectionUrl: 'https://css-tst.unlv.nevada.edu/PSIGW/PeopleSoftServiceListeningConnector/CI_UNLAUTH_BASIC.1.wsdl',
+        UNLAuth: {
+            UNL_FIELD1:'1000195910',
+            UNL_FIELD2:'123unlv!',
+            UNL_FIELD3:'N',
+            UNL_AUTH_TYPE:'ST',
+            UNL_APP_TYPE:'ITTEST',
+            UNL_KEY:'MYTEST'
+        }
+    }
+  };
---
+++ index.js
@@ -0,0 +1,18 @@
+const sdk = require('kinvey-flex-sdk');
+
+const handlers = require ('./lib/handlers');
+
+sdk.service((err, flex) => {
+  if (err) {
+    console.log('Error initializing the Flex SDK, exiting.');
+    throw err;
+  }
+
+  handlers.initHandler(flex.logger);
+
+  const data = flex.data;   // gets the datalink object from the service
+  const peopleSoftFlex = data.serviceObject('UNLV_People_Soft');
+
+  // wire up the events that we want to process
+  peopleSoftFlex.onGetByQuery(handlers.fetchHandler);
+});
